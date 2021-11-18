7070af5ce0132e43bb5ca371614b456ab1800e3b
Técnico Informática <56121742019.1@senacrjedu.local>
2021-11-18 20:49:10 -0300

    Update global.js

--- global.js
+++ global.js
@@ -5,14 +5,32 @@ $(document).ready(runApp);
 function runApp() {
 
     // Monitora cliques nas tags <a> do documento
-    $('a').click(() => { console.log('ping')});
+    $('a').click(routerLink);
 
 }
 
 // Processa cliques nas tags <a> do documento
 function routerLink() {
 
-    console.log(href, target)
+    // Obtém atributo 'href' do link clicado
+    var href = $(this).attr('href');
+
+    // Obtém atributo 'target' do link clicado
+    var target = $(this).attr('target');
+
+    // Se 'href' está vazio ou não existe (undefined), não faz nada
+    if (!href || href === '') return false;
+
+    // Trata os tipos de link
+    if (
+        href.substr(0, 7) === 'http://' ||  // Se é um link externo 'http' ou
+        href.substr(0, 8) === 'https://' || // Se é um link externo 'https' ou
+        target === '_blank' ||              // Se o 'target' é '_blank' ou
+        href.substr(0, 1) === '#'           // Se é uma âncora
+    ) return true;                          // Retorna o controla para o HTML
+
+    // Se é uma rota, chama a função 'loadPage', passando a rota
+    loadPage(href);
 
     // Conclui sem fazer nada
     return false;
